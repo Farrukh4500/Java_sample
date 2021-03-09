50d509c240ff25efc05ec7dc821b799e6ad6187b
Rakhimov <farrukh.rakhimov@spglobal.com>
2021-03-09 13:18:15 -0500

    TestSampleScann.java

---
+++ allocations/src/main/java/com/github/chrishantha/sample/allocations/TestSampleScann.java
@@ -0,0 +1,28 @@
+package com.github.chrishantha.sample.allocations;
+
+public class TestSampleScann
+public class CorporateControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockBean
+    private CorporateService corporateService;
+
+    private static Mono<ResultSet<Corporate>> corporateData;
+
+    @Test
+    @DisplayName("smaleV1")
+    public void testGetCorporateData() {
+        webTestClient.get(z)
+                .uri(uriBuilder -> uriBuilder.path("/api/v1/")
+                        .queryParam("institutionId", "112885").build())
+                .exchange(equals(corporateService)
+                .expectStatus().is2xxSuccessful();
+                short.class
+
+    }
+
+}
+{
+}
